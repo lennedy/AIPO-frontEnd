814038c8dc73458a530db1f33f4788f089e76ca6
lennedy <lennedy@gmail.com>
2025-04-06 19:51:41 -0300

    Nova funcionalidade
    Inicio da implementação da funcionalidade para enviar autorização de um ou mais usuários para uma sala específica

---
+++ src/layouts/roomboard/components/ListToAuthorize/index.js
@@ -0,0 +1,139 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Card from "@mui/material/Card";
+import Icon from "@mui/material/Icon";
+import Tooltip from "@mui/material/Tooltip";
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import SendIcon from "@mui/icons-material/Send";
+
+// react-routers components
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+// prop-types is library for typechecking of props
+import PropTypes from "prop-types";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDAvatar from "components/MDAvatar";
+import MDButton from "components/MDButton";
+
+import DataTable from "examples/Tables/DataTable";
+import projectsTableData from "layouts/tables/data/projectsTableData";
+import UsersTableData from "../../data/UsersTableData";
+
+import { useMaterialUIController } from "context";
+
+function AuthorizedUsers({ title, profiles, shadow }) {
+  const [controller] = useMaterialUIController();
+  const { darkMode } = controller;
+
+  const [editEnable, setEditEnable] = useState(false);
+  const [configToSend, setConfigToSend] = useState(false);
+  const [usersEdit, setUsersEdit] = useState({});
+
+  const autorizados = UsersTableData(profiles.codigo, editEnable, usersEdit);
+
+  var { columns: pColumns, rows: pRows, usersToEdit: pUsersToEdit } = autorizados;
+
+  const { columns: edColumns, rows: edRows } = autorizados.usersToEdit;
+
+  console.log("usuariosAutorizados");
+
+  // if (autorizados.usersToEdit != usersEdit) {
+  //   console.log(autorizados.usersToEdit);
+  //   setUsersEdit(autorizados.usersToEdit);
+  // }
+
+  const handleEditClick = (event) => {
+    if (configToSend) {
+      setEditEnable(true);
+    } else {
+      setEditEnable(!editEnable);
+    }
+    setConfigToSend(false);
+  };
+
+  const handleSendClick = (event) => {
+    setConfigToSend(true);
+    console.log("enviei os dados");
+    console.log(edColumns);
+    console.log(autorizados.usersToEdit);
+  };
+
+  return (
+    <Card sx={{ height: "100%", boxShadow: !shadow && "none" }}>
+      <MDBox pt={2} px={2}>
+        <MDBox pt={2} px={2} display="flex" justifyContent="space-between" alignItems="center">
+          <MDTypography variant="h6" fontWeight="medium" textTransform="capitalize">
+            {title}
+          </MDTypography>
+          <MDBox ml="auto" lineHeight={0} color={darkMode ? "white" : "dark"}>
+            <Tooltip title="Selecionar autorização" placement="top">
+              <IconButton sx={{ cursor: "pointer" }} fontSize="small" onClick={handleEditClick}>
+                <EditIcon />
+              </IconButton>
+            </Tooltip>
+          </MDBox>
+          <MDBox ml="auto" lineHeight={0} color={darkMode ? "white" : "dark"}>
+            <Tooltip title="Enviar Autorização" placement="top">
+              <IconButton sx={{ cursor: "pointer" }} fontSize="small" onClick={handleSendClick}>
+                <SendIcon />
+              </IconButton>
+            </Tooltip>
+          </MDBox>
+        </MDBox>
+      </MDBox>
+      {configToSend == false ? (
+        <DataTable
+          table={{ columns: pColumns, rows: pRows }}
+          isSorted={false}
+          entriesPerPage={false}
+          showTotalEntries={false}
+          canSearch
+          noEndBorder
+        />
+      ) : (
+        <DataTable
+          table={{ columns: edColumns, rows: edRows }}
+          isSorted={false}
+          entriesPerPage={false}
+          showTotalEntries={false}
+          noEndBorder
+        />
+      )}
+    </Card>
+  );
+}
+
+// Setting default props for the RoomList
+AuthorizedUsers.defaultProps = {
+  shadow: true,
+};
+
+// Typechecking props for the RoomList
+AuthorizedUsers.propTypes = {
+  title: PropTypes.string.isRequired,
+  profiles: PropTypes.arrayOf(PropTypes.object).isRequired,
+  shadow: PropTypes.bool,
+};
+
+export default AuthorizedUsers;
--- src/layouts/roomboard/data/roomData.js
+++ src/layouts/roomboard/data/roomData.js
@@ -6,12 +6,16 @@ import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 
 import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
 import Footer from "examples/Footer";
 import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import MDButton from "components/MDButton";
 
 import roomListData from "layouts/profile/data/roomListData";
 import RoomList from "examples/Lists/RoomList";
 import AuthorizedUsers from "../components/AuthorizedUsers";
+import ListToAuthorize from "../components/ListToAuthorize";
 import LasAcessOverview from "layouts/profile/components/LastAcessOverview";
 import PaymentMethod from "layouts/billing/components/PaymentMethod";
 import RoomHead from "layouts/roomboard/components/RoomHead";
@@ -21,8 +25,12 @@ import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
 
 import DataTable from "examples/Tables/DataTable";
 import authorsTableData from "layouts/tables/data/authorsTableData";
+import RoomInfo from "layouts/roomboard/components/RoomInfo";
+import robotica from "assets/images/lab-robotica.jpeg";
+import { DateRangePicker, DateRange } from "mui-daterange-picker";
 
 import { useMaterialUIController } from "context";
+import { useState } from "react";
 
 function RoomData() {
   const [controller] = useMaterialUIController();
@@ -32,6 +40,14 @@ function RoomData() {
   const dataChart = reportsBarChartData();
   const authorizedUsers = [];
   const { columns: pColumns, rows: pRows } = authorsTableData();
+  const codigo = location.state.codigo;
+  const nome = location.state.nome;
+  const [addAuthorization, setAddAuthorization] = useState(false);
+
+  const handleClick = (event) => {
+    console.log(nome);
+    setAddAuthorization(!addAuthorization);
+  };
 
   return (
     <DashboardLayout>
@@ -42,18 +58,61 @@ function RoomData() {
             <Grid item xs={12} lg={8}>
               <Grid container spacing={3}>
                 <Grid item xs={12}>
-                  <RoomHead codigo={location.state.codigo} nome={location.state.nome} />
+                  {/* <RoomHead codigo={location.state.codigo} nome={location.state.nome} /> */}
+                  <Card id="delete-account">
+                    <MDBox
+                      pt={2}
+                      px={2}
+                      display="flex"
+                      justifyContent="space-between"
+                      alignItems="center"
+                    >
+                      <Grid>
+                        {codigo == null ? (
+                          <MDTypography variant="h6" fontWeight="medium">
+                            Salas
+                          </MDTypography>
+                        ) : (
+                          <RoomInfo image={robotica} name={nome} codigo={codigo} />
+                        )}
+                      </Grid>
+                      <Grid>
+                        <MDBox>
+                          <MDButton variant="gradient" color="dark" onClick={handleClick}>
+                            <Icon sx={{ fontWeight: "bold" }}>add</Icon>
+                            &nbsp;{codigo == null ? "Adicionar Sala" : "Autorizar acesso"}
+                          </MDButton>
+                        </MDBox>
+                      </Grid>
+                    </MDBox>
+                    <MDBox p={1}>
+                      <Grid container spacing={3}></Grid>
+                    </MDBox>
+                  </Card>
                 </Grid>
-                <Grid item xs={12} md={6} xl={6}>
-                  {<AuthorizedUsers title={"Usuários Autorizados"} profiles={authorizedUsers} />}
-                </Grid>
-                <Grid item xs={12} md={6} xl={6}>
-                  <LasAcessOverview
-                    title="Últimos acessos"
-                    profiles={roomListData}
-                    shadow={false}
-                  />
-                </Grid>
+                {!addAuthorization ? (
+                  <Grid item xs={12} md={6} xl={6}>
+                    {<AuthorizedUsers title={"Usuários Autorizados"} profiles={location.state} />}
+                  </Grid>
+                ) : (
+                  <Grid item xs={12} md={6} xl={6}>
+                    {
+                      <ListToAuthorize
+                        title={"Usuários para Autorizar"}
+                        profiles={location.state}
+                      />
+                    }
+                  </Grid>
+                )}
+                {!addAuthorization ? (
+                  <Grid item xs={12} md={6} xl={6}>
+                    <LasAcessOverview
+                      title="Últimos acessos"
+                      profiles={roomListData}
+                      shadow={false}
+                    />
+                  </Grid>
+                ) : null}
               </Grid>
             </Grid>
           </Grid>
