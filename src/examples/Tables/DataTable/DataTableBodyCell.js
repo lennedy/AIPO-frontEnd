bc98fca28012c13d6801572f685ae7b8b1175105
lennedy <lennedy@gmail.com>
2025-04-18 18:23:45 -0300

    Remoção de bug
    modificação de código para permitir que que uma coluna possa ser ocultada e mesmo assim não fique como um espaço vazio na tabela.

--- src/examples/Tables/DataTable/DataTableBodyCell.js
+++ src/examples/Tables/DataTable/DataTableBodyCell.js
@@ -19,34 +19,39 @@ import PropTypes from "prop-types";
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 
-function DataTableBodyCell({ noBorder, align, children }) {
-  return (
-    <MDBox
-      component="td"
-      textAlign={align}
-      py={1.5}
-      px={3}
-      sx={({ palette: { light }, typography: { size }, borders: { borderWidth } }) => ({
-        fontSize: size.sm,
-        borderBottom: noBorder ? "none" : `${borderWidth[1]} solid ${light.main}`,
-      })}
-    >
+function DataTableBodyCell({ noBorder, align, children, hidden }) {
+  if (hidden) {
+    return null;
+  } else {
+    return (
       <MDBox
-        display="inline-block"
-        width="max-content"
-        color="text"
-        sx={{ verticalAlign: "middle" }}
+        component="td"
+        textAlign={align}
+        py={1.5}
+        px={3}
+        sx={({ palette: { light }, typography: { size }, borders: { borderWidth } }) => ({
+          fontSize: size.sm,
+          borderBottom: noBorder ? "none" : `${borderWidth[1]} solid ${light.main}`,
+        })}
       >
-        {children}
+        <MDBox
+          display="inline-block"
+          width="max-content"
+          color="text"
+          sx={{ verticalAlign: "middle" }}
+        >
+          {children}
+        </MDBox>
       </MDBox>
-    </MDBox>
-  );
+    );
+  }
 }
 
 // Setting default values for the props of DataTableBodyCell
 DataTableBodyCell.defaultProps = {
   noBorder: false,
   align: "left",
+  hidden: false,
 };
 
 // Typechecking props for the DataTableBodyCell
@@ -54,6 +59,7 @@ DataTableBodyCell.propTypes = {
   children: PropTypes.node.isRequired,
   noBorder: PropTypes.bool,
   align: PropTypes.oneOf(["left", "right", "center"]),
+  hidden: PropTypes.bool,
 };
 
 export default DataTableBodyCell;
