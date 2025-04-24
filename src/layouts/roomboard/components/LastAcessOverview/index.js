e71349bca2924591df4ab816167fc27915243729
lennedy <lennedy@gmail.com>
2025-04-23 21:02:07 -0300

    Remoção de código comentado

--- src/layouts/roomboard/components/LastAcessOverview/index.js
+++ src/layouts/roomboard/components/LastAcessOverview/index.js
@@ -114,27 +114,6 @@ function LasAcessOverview({ codigo_salas }) {
         }
       })
       .catch((err) => console.log(err));
-
-    // fetch(api.database + "/acessosPorUsuario/" + authData.user.matricula, {
-    //   method: "POST",
-    //   body: JSON.stringify(data_inicia_final),
-    //   headers: { "Content-type": "application/json; charset=UTF-8" },
-    // })
-    //   .then((response) => response.json())
-    //   .then((json) => {
-    //     if (json["status"] == "ok") {
-    //       const data = json["data"];
-    //       let numAcessos = 0;
-    //       for (let key in data) {
-    //         console.log(data[key]);
-    //         numAcessos = data[key] + numAcessos;
-    //       }
-    //       setAcessosMensais(numAcessos);
-    //     } else {
-    //       alert("erro:" + json["status"]);
-    //     }
-    //   })
-    //   .catch((err) => console.log(err));
   }, []);
 
   console.log("TEste");
@@ -166,92 +145,6 @@ function LasAcessOverview({ codigo_salas }) {
             lastItem={i + 1 >= ultimosAcessos.length ? true : false}
           />
         ))}
-        {/* <TimelineItem
-          color={
-            ultimosAcessos.length >= 5
-              ? ultimosAcessos[ultimosAcessos.length - 5].autorizado
-                ? "success"
-                : "error"
-              : "error"
-          }
-          icon="vpn_key"
-          title={
-            ultimosAcessos.length >= 5 ? ultimosAcessos[ultimosAcessos.length - 5].nomeSala : ""
-          }
-          dateTime={
-            ultimosAcessos.length >= 5 ? ultimosAcessos[ultimosAcessos.length - 5].diahorario : "0"
-          }
-          display={ultimosAcessos.length >= NUMERO_MAXIMO_ACESSOS_PARA_EXIBIR ? true : false}
-        />
-        <TimelineItem
-          color={
-            ultimosAcessos.length >= 4
-              ? ultimosAcessos[ultimosAcessos.length - 4].autorizado
-                ? "success"
-                : "error"
-              : "error"
-          }
-          icon="vpn_key"
-          title={
-            ultimosAcessos.length >= 4 ? ultimosAcessos[ultimosAcessos.length - 4].nomeSala : ""
-          }
-          dateTime={
-            ultimosAcessos.length >= 4 ? ultimosAcessos[ultimosAcessos.length - 4].diahorario : "0"
-          }
-          display={ultimosAcessos.length >= NUMERO_MAXIMO_ACESSOS_PARA_EXIBIR - 1 ? true : false}
-        />
-        <TimelineItem
-          color={
-            ultimosAcessos.length >= 3
-              ? ultimosAcessos[ultimosAcessos.length - 3].autorizado
-                ? "success"
-                : "error"
-              : "error"
-          }
-          icon="vpn_key"
-          title={
-            ultimosAcessos.length >= 3 ? ultimosAcessos[ultimosAcessos.length - 3].nomeSala : ""
-          }
-          dateTime={
-            ultimosAcessos.length >= 3 ? ultimosAcessos[ultimosAcessos.length - 3].diahorario : "0"
-          }
-          display={ultimosAcessos.length >= NUMERO_MAXIMO_ACESSOS_PARA_EXIBIR - 2 ? true : false}
-        />
-        <TimelineItem
-          color={
-            ultimosAcessos.length >= 2
-              ? ultimosAcessos[ultimosAcessos.length - 2].autorizado
-                ? "success"
-                : "error"
-              : "error"
-          }
-          icon="vpn_key"
-          title={
-            ultimosAcessos.length >= 2 ? ultimosAcessos[ultimosAcessos.length - 2].nomeSala : ""
-          }
-          dateTime={
-            ultimosAcessos.length >= 2 ? ultimosAcessos[ultimosAcessos.length - 2].diahorario : "0"
-          }
-          display={ultimosAcessos.length >= NUMERO_MAXIMO_ACESSOS_PARA_EXIBIR - 3 ? true : false}
-        />
-        <TimelineItem
-          color={
-            ultimosAcessos.length >= 1
-              ? ultimosAcessos[ultimosAcessos.length - 1].autorizado
-                ? "success"
-                : "error"
-              : "error"
-          }
-          icon="vpn_key"
-          title={
-            ultimosAcessos.length >= 1 ? ultimosAcessos[ultimosAcessos.length - 1].nomeSala : ""
-          }
-          dateTime={
-            ultimosAcessos.length >= 1 ? ultimosAcessos[ultimosAcessos.length - 1].diahorario : "0"
-          }
-          lastItem
-          display={ultimosAcessos.length >= NUMERO_MAXIMO_ACESSOS_PARA_EXIBIR - 4 ? true : false}
-        /> */}
       </MDBox>
     </Card>
   );
