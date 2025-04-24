6ba50ee3e4103215e866038fa14d55ce002c8280
lennedy <lennedy@gmail.com>
2025-04-24 15:50:23 -0300

    Simplificação de código
    Utilização do map pois para simplificar o código

--- src/layouts/profile/components/LastAcessOverview/index.js
+++ src/layouts/profile/components/LastAcessOverview/index.js
@@ -151,92 +151,16 @@ function LasAcessOverview() {
         </MDBox>
       </MDBox>
       <MDBox p={2}>
-        <TimelineItem
-          color={
-            ultimosAcessos.length >= 5
-              ? ultimosAcessos[ultimosAcessos.length - 5].autorizado
-                ? "success"
-                : "error"
-              : "error"
-          }
-          icon="vpn_key"
-          title={
-            ultimosAcessos.length >= 5 ? ultimosAcessos[ultimosAcessos.length - 5].nomeSala : ""
-          }
-          dateTime={
-            ultimosAcessos.length >= 5 ? ultimosAcessos[ultimosAcessos.length - 5].diahorario : "0"
-          }
-          display={ultimosAcessos.length >= NUMERO_MAXIMO_ACESSOS_PARA_EXIBIR ? true : false}
-        />
-        <TimelineItem
-          color={
-            ultimosAcessos.length >= 4
-              ? ultimosAcessos[ultimosAcessos.length - 4].autorizado
-                ? "success"
-                : "error"
-              : "error"
-          }
-          icon="vpn_key"
-          title={
-            ultimosAcessos.length >= 4 ? ultimosAcessos[ultimosAcessos.length - 4].nomeSala : ""
-          }
-          dateTime={
-            ultimosAcessos.length >= 4 ? ultimosAcessos[ultimosAcessos.length - 4].diahorario : "0"
-          }
-          display={ultimosAcessos.length >= NUMERO_MAXIMO_ACESSOS_PARA_EXIBIR - 1 ? true : false}
-        />
-        <TimelineItem
-          color={
-            ultimosAcessos.length >= 3
-              ? ultimosAcessos[ultimosAcessos.length - 3].autorizado
-                ? "success"
-                : "error"
-              : "error"
-          }
-          icon="vpn_key"
-          title={
-            ultimosAcessos.length >= 3 ? ultimosAcessos[ultimosAcessos.length - 3].nomeSala : ""
-          }
-          dateTime={
-            ultimosAcessos.length >= 3 ? ultimosAcessos[ultimosAcessos.length - 3].diahorario : "0"
-          }
-          display={ultimosAcessos.length >= NUMERO_MAXIMO_ACESSOS_PARA_EXIBIR - 2 ? true : false}
-        />
-        <TimelineItem
-          color={
-            ultimosAcessos.length >= 2
-              ? ultimosAcessos[ultimosAcessos.length - 2].autorizado
-                ? "success"
-                : "error"
-              : "error"
-          }
-          icon="vpn_key"
-          title={
-            ultimosAcessos.length >= 2 ? ultimosAcessos[ultimosAcessos.length - 2].nomeSala : ""
-          }
-          dateTime={
-            ultimosAcessos.length >= 2 ? ultimosAcessos[ultimosAcessos.length - 2].diahorario : "0"
-          }
-          display={ultimosAcessos.length >= NUMERO_MAXIMO_ACESSOS_PARA_EXIBIR - 3 ? true : false}
-        />
-        <TimelineItem
-          color={
-            ultimosAcessos.length >= 1
-              ? ultimosAcessos[ultimosAcessos.length - 1].autorizado
-                ? "success"
-                : "error"
-              : "error"
-          }
-          icon="vpn_key"
-          title={
-            ultimosAcessos.length >= 1 ? ultimosAcessos[ultimosAcessos.length - 1].nomeSala : ""
-          }
-          dateTime={
-            ultimosAcessos.length >= 1 ? ultimosAcessos[ultimosAcessos.length - 1].diahorario : "0"
-          }
-          lastItem
-          display={ultimosAcessos.length >= NUMERO_MAXIMO_ACESSOS_PARA_EXIBIR - 4 ? true : false}
-        />
+        {ultimosAcessos.map((acesso, i, ultimosAcessos) => (
+          <TimelineItem
+            key={acesso.matricula}
+            color={acesso.autorizado ? "success" : "error"}
+            icon="vpn_key"
+            title={acesso.nomeSala}
+            dateTime={acesso.diahorario}
+            lastItem={i + 1 >= ultimosAcessos.length ? true : false}
+          />
+        ))}
       </MDBox>
     </Card>
   );
