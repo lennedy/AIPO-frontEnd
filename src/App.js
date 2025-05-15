7513c71173bf502a13aa8fa1d4fd1ad32f9582a6
lennedy <lennedy@gmail.com>
2025-05-15 19:54:08 -0300

    Mudança do nome do programa na interface

--- src/App.js
+++ src/App.js
@@ -217,7 +217,7 @@ export default function App() {
             <Sidenav
               color={sidenavColor}
               brand={(transparentSidenav && !darkMode) || whiteSidenav ? brandDark : brandWhite}
-              brandName="Material Dashboard 2"
+              brandName="AIPO"
               routes={routes}
               onMouseEnter={handleOnMouseEnter}
               onMouseLeave={handleOnMouseLeave}
