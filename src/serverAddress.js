ac75e12c8f93381eaf9253df50283e105ea77b05
lennedy <lennedy@gmail.com>
2025-05-27 10:57:30 -0300

    Mudança da leitura serial do servidor para a máquina local

--- src/serverAddress.js
+++ src/serverAddress.js
@@ -1,7 +1,7 @@
 // endereço do servidor onde está a API
 export default function getApiAddress() {
   // const apiAddress = { database: "https://192.168.1.29/api", serial: "localhost" };
-  const apiAddress = { database: "http://127.0.0.1:5000", serial: "localhost" };
+  const apiAddress = { database: "http://127.0.0.1:5000", serial: "http://127.0.0.1:8000" };
   return apiAddress;
 }
 
