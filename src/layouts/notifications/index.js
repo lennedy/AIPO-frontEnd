6a72a6a8a134a7bf682a728bc45f64cbc8444755
Lennedy <lennedy.soares@ifrn.edu.br>
2025-12-15 04:48:55 -0300

    modificações para fazer conexão com o socket.io-clientso

--- src/layouts/notifications/index.js
+++ src/layouts/notifications/index.js
@@ -13,7 +13,9 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import { io } from "socket.io-client";
 
 // @mui material components
 import Grid from "@mui/material/Grid";
@@ -45,6 +47,37 @@ function Notifications() {
   const closeWarningSB = () => setWarningSB(false);
   const openErrorSB = () => setErrorSB(true);
   const closeErrorSB = () => setErrorSB(false);
+
+  const SOCKET_URL = "http://localhost:5000";
+
+  const socket = io(SOCKET_URL);
+
+  useEffect(() => {
+    socket.onAny((event, ...args) => {
+      console.log("[socket.io] evento recebido:", event, args);
+    });
+
+    socket.on("connect", () => {
+      console.log("Conectado ao backend via WebSocket. ID:", socket.id);
+    });
+
+    socket.on("server_message", (msg) => {
+      console.log("Mensagem do servidor:", msg);
+    });
+
+    socket.on("mqtt_message", (payload) => {
+      console.log("Recebi do backend (mqtt_message):", payload);
+      setLastMessage(payload);
+      setMessages((old) => [payload, ...old]);
+    });
+
+    return () => {
+      socket.offAny();
+      socket.off("connect");
+      socket.off("server_message");
+      socket.off("mqtt_message");
+    };
+  }, []);
 
   const alertContent = (name) => (
     <MDTypography variant="body2" color="white">
